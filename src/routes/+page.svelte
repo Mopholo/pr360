cacf038f192acb019968b8bf1202e4d9e62e2c0c
Todd Werelius <todd.werelius@gmail.com>
2022-09-09 13:44:55 -0700

    udpate

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	import PillButton from '$uikit/pillButton.svelte';
+</script>
+
+<h1>Component Playground</h1>
+
+<PillButton id="task-list" name="tasks" value="route-A" />
+<PillButton id="task-kanban" name="tasks" value="route-B" />
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,26 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		alias: {
+			$src: 'src',
+			$lib: 'src/lib',
+			$uikit: 'src/components/uikit',
+			$views: 'src/components/views',
+			$forms: 'src/conponents/forms',
+			$stores: 'src/stores',
+			$api: 'src/api',
+		}
+	},
+
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+	]
 };
 
 export default config;
